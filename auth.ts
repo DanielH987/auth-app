7f4a8b871fd4a04f1563234d8e2bd1929e4e07d1
DanielH987 <hootinid@go.byuh.edu>
2024-05-28 14:47:16 -1000

    Enhance NextAuth configuration to include user role in session and JWT

--- auth.ts
+++ auth.ts
@@ -2,7 +2,9 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
- 
+import { getUserById } from "@/data/user";
+import { UserRole } from "@prisma/client";
+
 export const { 
   handlers: { GET, POST },
   auth, 
@@ -11,13 +13,25 @@ export const {
 } = NextAuth({
   callbacks: {
     async session({ token, session }) {
+      if (token.sub && session.user) {
+      session.user.id = token.sub;
+      }
 
-       if (token.sub && session.user) {
-        session.user.id = token.sub;
-       }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
       return session;
     }, 
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     }
   },
