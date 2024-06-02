998c843f427a1c01968924962309cb650f7cea11
DanielH987 <hootinid@go.byuh.edu>
2024-06-01 19:14:43 -1000

    Handle OAuthAccountNotLinked error in LoginForm component

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as z from "zod";
+import { useSearchParams } from "next/navigation";
 import { useState, useTransition } from "react";
 import { LoginSchema } from "@/schemas";
 import { useForm } from "react-hook-form";
@@ -21,6 +22,11 @@ import { CardWrapper } from "@/components/auth/card-wrapper";
 import { login } from "@/actions/login";
 
 export const LoginForm = () => {
+    const searchParams = useSearchParams();
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
+        ? "Email already in use with different provider!"
+        : "";
+
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>(""); 
     const [isPending, startTransition] = useTransition();
@@ -40,8 +46,8 @@ export const LoginForm = () => {
         startTransition(() => {
             login(values)
                 .then((data) => {
-                    setError(data.error);
-                    setSuccess(data.success);
+                    setError(data?.error);
+                    // setSuccess(data?.success);
                 });
         });
     };
@@ -96,7 +102,7 @@ export const LoginForm = () => {
                             )}  
                         />
                     </div>
-                    <FormError message={error}/>
+                    <FormError message={error || urlError}/>
                     <FormSuccess message={success}/>
                     <Button
                         disabled={isPending}
