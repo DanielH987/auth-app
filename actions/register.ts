a1bef9d340aeb3e47b858aafdab673e6c1e38aad
DanielH987 <hootinid@go.byuh.edu>
2024-06-02 22:25:36 -1000

    Generate verification token when registering using credentials

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcryptjs";
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
 
 export const register = async  (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -30,7 +31,9 @@ export const register = async  (values: z.infer<typeof RegisterSchema>) => {
         }
     });
 
+    const verificationToken = await generateVerificationToken(email);
+
     // TODO: send verification token email
  
-    return { success: "User created!" };
+    return { success: "Confirmation email sent!" };
 }
