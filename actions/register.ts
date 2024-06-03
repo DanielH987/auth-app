b1a5aabf54f7bd7d2810708551b6f21ec4993f54
DanielH987 <hootinid@go.byuh.edu>
2024-06-02 23:08:46 -1000

    Integrate sendVerificationEmail function to trigger email sending when registering

--- actions/register.ts
+++ actions/register.ts
@@ -6,6 +6,7 @@ import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async  (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -33,7 +34,10 @@ export const register = async  (values: z.infer<typeof RegisterSchema>) => {
 
     const verificationToken = await generateVerificationToken(email);
 
-    // TODO: send verification token email
+    await sendVerificationEmail(
+        verificationToken.email,
+        verificationToken.token,
+    );
  
     return { success: "Confirmation email sent!" };
 }
