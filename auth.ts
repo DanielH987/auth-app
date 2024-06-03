cd7d0952ad38c41d6081acbb5e84e4b86046f779
DanielH987 <hootinid@go.byuh.edu>
2024-06-02 23:09:38 -1000

    Add email verification check in signIn callback for credentials provider

--- auth.ts
+++ auth.ts
@@ -24,6 +24,17 @@ export const {
     }
   },
   callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") return true;
+
+      const existingUser = await getUserById(user.id);
+
+      if (!existingUser?.emailVerified) return false;
+
+      //TODO: Add 2FA check
+
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
       session.user.id = token.sub;
