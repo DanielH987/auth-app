ac8fcc01bab5e81c415b02ea52a417cc456372b2
DanielH987 <hootinid@go.byuh.edu>
2024-06-02 23:08:07 -1000

    Set success message when Login in

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -47,7 +47,7 @@ export const LoginForm = () => {
             login(values)
                 .then((data) => {
                     setError(data?.error);
-                    // setSuccess(data?.success);
+                    setSuccess(data?.success);
                 });
         });
     };
