5c647350e15d5e3c9ea415b52028dfcfa15ba578
DanielH987 <hootinid@go.byuh.edu>
2024-06-04 09:07:56 -1000

    Retrieve token on new verification form page

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import { useCallback, useEffect } from "react";
 import { PropagateLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 
 export const NewVerificationForm = () => {
+    const searchParams = useSearchParams();
+
+    const token = searchParams.get("token");
+
+    const onSubmit = useCallback(() => {
+        console.log(token);
+    }, [token]);
+
+    useEffect(() => {
+        onSubmit();
+    }, [onSubmit]);
+
     return (
         <CardWrapper
             headerLabel="Confirm your verification"
