0df19daa21c90ab2b20882996892a8dc92e2bb31
DanielH987 <hootinid@go.byuh.edu>
2024-06-07 19:30:57 -1000

    Add 2FA check in login handler

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
 import { getUserById } from "@/data/user";
 import { UserRole } from "@prisma/client";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
 
 export const { 
   handlers: { GET, POST },
@@ -31,7 +32,16 @@ export const {
 
       if (!existingUser?.emailVerified) return false;
 
-      //TODO: Add 2FA check
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
+
+        if (!twoFactorConfirmation) return false;
+
+        // Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id }
+        });
+      }
 
       return true;
     },
