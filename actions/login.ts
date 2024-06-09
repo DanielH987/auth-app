582a41af0fc06504e87e751455b482296b4905a2
DanielH987 <hootinid@go.byuh.edu>
2024-06-08 22:56:56 -1000

    feat: Add two-factor authentication support to login process

--- actions/login.ts
+++ actions/login.ts
@@ -5,9 +5,9 @@ import { LoginSchema } from "@/schemas"
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
-import { generateVerificationToken } from "@/lib/tokens";
 import { getUserByEmail } from "@/data/user";
-import { sendVerificationEmail } from "@/lib/mail";
+import { generateVerificationToken, generateTwoFactorToken } from "@/lib/tokens";
+import { sendVerificationEmail, sendTwoFactorTokenEmail } from "@/lib/mail";
 
 export const login = async  (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
@@ -33,6 +33,17 @@ export const login = async  (values: z.infer<typeof LoginSchema>) => {
         );
 
         return { success: "Confirmation email sent!" };
+    }
+
+    if (existingUser.isTwoFactorEnabled && existingUser.email) {
+        const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+
+        await sendTwoFactorTokenEmail(
+            twoFactorToken.email,
+            twoFactorToken.token,
+        );
+
+        return { twoFactor: true };
     }
 
     try {
