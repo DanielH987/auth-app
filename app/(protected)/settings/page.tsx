a862c8741a7fb350cdf4a27ad262d872d6af65b3
DanielH987 <hootinid@go.byuh.edu>
2024-06-09 23:07:38 -1000

    refactor: Update sign out functionality in SettingsPage component

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { useSession, signOut } from "next-auth/react";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { signOut } from "next-auth/react";
 
 const SettingsPage = () => {
-  const session = useSession();
+  const user = useCurrentUser();
 
   const onClick = () => {
     signOut();
   };
 
  return (
-    <div>
-        {JSON.stringify(session)}
+    <div className="bg-white p-10 rounded-xl">
         <button onClick={onClick} type="submit">
           Sign out
         </button>
