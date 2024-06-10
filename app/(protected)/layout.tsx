eef3487480a9f8e8f77f3354bcf9c095af16b95d
DanielH987 <hootinid@go.byuh.edu>
2024-06-09 23:07:51 -1000

    feat: Add ProtectedLayout component for protected routes

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,13 @@
+interface ProtectedLayoutProps {
+    children: React.ReactNode;
+};
+
+const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
+ return ( 
+    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+        {children}
+    </div>
+ );
+}
+
+export default ProtectedLayout;
