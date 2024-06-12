7a05320d045492167f6a816619362641dc0a4a01
DanielH987 <hootinid@go.byuh.edu>
2024-06-11 23:36:43 -1000

    feat: Add getAccountByUserId function to retrieve user account information

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { db } from "@/lib/db";
 import { getUserById } from "@/data/user";
 import { UserRole } from "@prisma/client";
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+import { getAccountByUserId } from "./data/account";
 
 export const { 
   handlers: { GET, POST },
@@ -57,15 +58,26 @@ export const {
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
       }
 
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
       return session;
     }, 
-    async jwt({ token }) {
+    async jwt({ token }) { 
       if (!token.sub) return token;
 
       const existingUser = await getUserById(token.sub);
 
       if (!existingUser) return token;
 
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
