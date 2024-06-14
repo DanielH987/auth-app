1fcbff561940a9cec67c22e665dbc69e50982609
DanielH987 <hootinid@go.byuh.edu>
2024-06-13 19:53:42 -1000

    refactor: Update login form to include callback URL parameter

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -24,6 +24,7 @@ import { login } from "@/actions/login";
 
 export const LoginForm = () => {
     const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get("callbackUrl");
     const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
         ? "Email already in use with different provider!"
         : "";
@@ -45,7 +46,7 @@ export const LoginForm = () => {
         setSuccess("");
 
         startTransition(() => {
-            login(values)
+            login(values, callbackUrl)
                 .then((data) => {
                     if (data?.error) {
                         form.reset();
