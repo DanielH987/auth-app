abbff50d8bb24e4012bb2b48aa96775bf8d012bd
DanielH987 <hootinid@go.byuh.edu>
2024-06-19 01:20:11 -1000

    refactor: Update mail.ts to use dynamic domain for confirmation link

--- lib/mail.ts
+++ lib/mail.ts
@@ -48,7 +48,7 @@ export const sendPasswordResetEmail = async (
 export const sendVerificationEmail = async (
     email: string, token: string
 ) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
         from: "onboarding@resend.dev",
